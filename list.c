d8beee4b7aa3a1d395dfe89795b8dd859956b28e
Louis Cruz <cruzlouis13@gmail.com>
2016-05-18 00:39:35 -0400

    format list and fix includes

--- list.c
+++ list.c
@@ -1,13 +1,10 @@
-#include <list.h>
+#include "list.h"
 
+List *List_create() { return calloc(1, sizeof(List)); }
 
-List *List_create(){
-  return calloc(1,sizeof(List));
-}
-
-void List_destroy(List *list){
-  LIST_FOREACH(list, first, next, cur){
-    if (cur->prev){
+void List_destroy(List *list) {
+  LIST_FOREACH(list, first, next, cur) {
+    if (cur->prev) {
       free(cur->value);
     }
   }
@@ -15,27 +12,24 @@ void List_destroy(List *list){
   free(list);
 }
 
-
-void List_clear(List *list){
-  LIST_FOREACH(list, first, next, cur){
-    free(cur->value);
-  }
+void List_clear(List *list) {
+  LIST_FOREACH(list, first, next, cur) { free(cur->value); }
 }
 
-void List_clear_destroy(List *list){
+void List_clear_destroy(List *list) {
   List_clear(list);
   List_destroy(list);
 }
 
-void List_push(List *list, void *value){
-  ListNode *node = calloc(1,sizeof(ListNode));
-  if(*node == NULL){
+void List_push(List *list, void *value) {
+  ListNode *node = calloc(1, sizeof(ListNode));
+  if (*node == NULL) {
     return;
   }
 
   node->value = value;
 
-  if(list->last == NULL){
+  if (list->last == NULL) {
     list->first = node;
     list->last = node;
   } else {
@@ -46,19 +40,19 @@ void List_push(List *list, void *value){
   list->count++;
 }
 
-void *List_pop(List * list){
+void *List_pop(List *list) {
   ListNode *node = list->last;
-  return node != NULL ? List_remove(list,node) : NULL;
+  return node != NULL ? List_remove(list, node) : NULL;
 }
 
-void List_unshift(List *list, void *value){
-  ListNode *node = calloc(1,sizeof(ListNode));
-  if(node == NULL){
+void List_unshift(List *list, void *value) {
+  ListNode *node = calloc(1, sizeof(ListNode));
+  if (node == NULL) {
     return;
   }
 
   node->value = value;
-  if(list->first == NULL){
+  if (list->first == NULL) {
     list->first = node;
     list->last = node;
   } else {
@@ -70,25 +64,25 @@ void List_unshift(List *list, void *value){
   list->count++;
 }
 
-void *List_shift(List *list){
+void *List_shift(List *list) {
   void ListNode *node = list->first;
-  return node != NULL ? List_remove(list,node) : NULL;
+  return node != NULL ? List_remove(list, node) : NULL;
 }
 
-void *List_remove(List *list, ListNode* node){
+void *List_remove(List *list, ListNode *node) {
   void *result = NULL;
 
-  if((list->first == NULL && list->last == NULL) || (node == NULL)){
+  if ((list->first == NULL && list->last == NULL) || (node == NULL)) {
     return;
   }
 
-  if(node == list->first && node == list->last){
+  if (node == list->first && node == list->last) {
     list->first = NULL;
     list->last = NULL;
-  } else if(node == list->first){
+  } else if (node == list->first) {
     list->first = node->next;
     list->first->prev = NULL;
-  } else if(node == list->last){
+  } else if (node == list->last) {
     list->last = node->prev;
     list->last->next = NULL;
   } else {
@@ -103,5 +97,4 @@ void *List_remove(List *list, ListNode* node){
   free(node);
 
   return result;
-
 }
--- list.h
+++ list.h
@@ -1,4 +1,4 @@
-#include <MainFile.h>;
+#include "MainFile.h"
 
 /*Definitions of linked list are leveraged from 'learn C the hard way'*/
 
@@ -21,7 +21,6 @@ void List_destroy(List *list);
 void List_clear(List *list);
 void List_clear_destroy(List *list);
 
-
 #define List_count(A) ((A)->count)
 #define List_first(A) ((A)->first != NULL ? (A)->first->value : NULL)
 #define List_last(A) ((A)->last != NULL ? (A)->last->value : NULL)
@@ -32,8 +31,9 @@ void *List_pop(List *list);
 void List_unshift(List *list, void *value);
 void *List_shift(List *list);
 
-
 void *List_remove(List *list, ListNode *node);
 
-#define LIST_FOREACH(L, S, M, V) ListNode *_node = NULL;\ ListNode *V = NULL;\
-for(V = _node = L-S; _node != NULL; V = _node =_node->M)
+#define LIST_FOREACH(L, S, M, V)                                               \
+  ListNode *_node = NULL;                                                      \
+  \ ListNode *V = NULL;                                                        \
+  for (V = _node = L - S; _node != NULL; V = _node = _node->M)
