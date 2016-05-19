b78901e191fd5fe3e7b109fdaee6d59b5979c361
Louis Cruz <cruzlouis13@gmail.com>
2016-05-19 14:17:39 -0400

    add comments to snake

--- snake.c
+++ snake.c
@@ -46,97 +46,102 @@ coordinate *coordinate_create(int x, int y) {
   return coord;
 }
 
-void coordinate_destroy(coordinate *coord) {
-  free(coord);
-}
+void coordinate_destroy(coordinate *coord) { free(coord); }
 
+/*Initializes the snake game*/
 void initGame(void) {
-	coordinate *tail1;
+  coordinate *tail1;
   coordinate *tail2;
   gameState = 1;
-	direction = 1;
-	score = 0;
-
+  direction = 1;
+  score = 0;
 
   snake = List_create();
 
   /*Create inital snake of 3 length at bottom left of display*/
   fruit = coordinate_create(5, 1);
   head = coordinate_create(4, 6);
-	tail1 = coordinate_create(4, 6);
-	tail2 = coordinate_create(4, 6);
-  
-	List_push(snake, head);
+  tail1 = coordinate_create(4, 6);
+  tail2 = coordinate_create(4, 6);
+
+  List_push(snake, head);
   List_push(snake, tail1);
   List_push(snake, tail2);
 }
 
-void moveBody(void){
-	ListNode *temp = snake->last;
-	while(temp->prev != NULL){
-		((coordinate *)temp->value)->x = ((coordinate*)temp->prev->value)->x;
-		((coordinate *)temp->value)->y = ((coordinate*)temp->prev->value)->y;
-		temp = temp->prev;
-	}
+/*Moves every node to its previous node's location*/
+void moveBody(void) {
+  ListNode *temp = snake->last;
+  while (temp->prev != NULL) {
+    ((coordinate *)temp->value)->x = ((coordinate *)temp->prev->value)->x;
+    ((coordinate *)temp->value)->y = ((coordinate *)temp->prev->value)->y;
+    temp = temp->prev;
+  }
 }
 
+/*Updates the head of and body of the snake in current direction*/
 void move(void) {
   switch (direction) {
   case 1:
     moveBody();
-		head->y--;
+    head->y--;
     break;
   case 2:
-		moveBody();
+    moveBody();
     head->x++;
     break;
   case 3:
     moveBody();
-		head->y++;
+    head->y++;
     break;
   case 4:
     moveBody();
-		head->x--;
+    head->x--;
   }
 }
 
+/*Generates a fruit in an open location*/
 void generateFruit(void) {
-	ListNode *temp;
-	int overlap;
-	
-	do {
-		temp = snake->first;
-		fruit->x = rand() % 7;
-		fruit->y = rand() % 7;
-		overlap = 0;
-		while(temp != NULL){
-			if(fruit->x == ((coordinate *)temp->value)->x && fruit->y == ((coordinate *)temp->value)->y){
-				overlap = 1;
-			}
-			temp = temp->next;
-		}
-	} while( overlap );	
+  ListNode *temp;
+  int overlap;
+
+  do {
+    temp = snake->first;
+    fruit->x = rand() % 7;
+    fruit->y = rand() % 7;
+    overlap = 0;
+    while (temp != NULL) {
+      if (fruit->x == ((coordinate *)temp->value)->x &&
+          fruit->y == ((coordinate *)temp->value)->y) {
+        overlap = 1;
+      }
+      temp = temp->next;
+    }
+  } while (overlap);
 }
 
+/*appends a segment to the snake*/
 void growSnake(void) {
   int x = ((coordinate *)snake->last->value)->x;
-  int y = ((coordinate * )snake->last->value)->y;
+  int y = ((coordinate *)snake->last->value)->y;
   coordinate *coord = coordinate_create(x, y);
   List_push(snake, coord);
-	score++;
+  score++;
 }
 
+/*Returns 1 if the head of the snake touches its body, 0 otherwise*/
 int selfCollision(void) {
   ListNode *temp = snake->first->next;
   while (temp != NULL) {
-    if (((coordinate*) temp->value)->x == head->x && ((coordinate *)temp->value)->y == head->y) {
+    if (((coordinate *)temp->value)->x == head->x &&
+        ((coordinate *)temp->value)->y == head->y) {
       return 1;
     }
     temp = temp->next;
   }
   return 0;
 }
-
+/*Updates the gameState depending on collision or eating fruit*/
 void checkState(void) {
 
   // snake out of bounds
@@ -150,30 +155,34 @@ void checkState(void) {
   }
 }
 
+/*updates the backdrop to reflect current game configuration*/
 void updateBoard(void) {
-	
-	ListNode *temp; 
-	temp = snake->first;
+
+  ListNode *temp;
+  temp = snake->first;
   clearBack();
-	
-	backdrop[((coordinate *)(temp->value))->x][((coordinate *)(temp->value))->y] = 3;
-	temp= temp->next;
-	
-	while(temp != NULL){
-		int x = ((coordinate *)(temp->value))->x;
-		int y = ((coordinate *)(temp->value))->y;
-		backdrop[x][y] = 2;
-		temp = temp->next;
-	}
+
+  backdrop[((coordinate *)(temp->value))->x][((coordinate *)(temp->value))->y] =
+      3;
+  temp = temp->next;
+
+  while (temp != NULL) {
+    int x = ((coordinate *)(temp->value))->x;
+    int y = ((coordinate *)(temp->value))->y;
+    backdrop[x][y] = 2;
+    temp = temp->next;
+  }
 
   backdrop[fruit->x][fruit->y] = 6;
 }
 
+/*Cleans up the games memory*/
 void endGame(void) {
   List_clear_destroy(snake);
   coordinate_destroy(fruit);
 }
 
+/*Checks if buttons were pressed and updates the current direction*/
 void checkUpdate(void) {
   int left = buttonsLast[0];
   int right = buttonsLast[1];
@@ -190,7 +199,7 @@ void checkUpdate(void) {
       break;
     case 4:
       direction = 1;
-			break;
+      break;
     default:
       direction = 4;
     }
@@ -207,13 +216,14 @@ void checkUpdate(void) {
       break;
     case 4:
       direction = 3;
-			break;
+      break;
     default:
       direction = 4;
     }
   }
 }
 
+/*Snake game loop*/
 int playSnake(void) {
   if (!gameState) {
     initGame();
@@ -223,14 +233,14 @@ int playSnake(void) {
   checkState();
   if (gameState == 0) {
     endGame();
-		sprintf(scorez, "SCORE %d", score);
-		buildScroller(scorez);
+    sprintf(scorez, "SCORE %d", score);
+    buildScroller(scorez);
   } else {
     updateBoard();
   }
 
   swapScreens();
-	delay(40000);
+  delay(40000);
 
   return gameState;
 }
